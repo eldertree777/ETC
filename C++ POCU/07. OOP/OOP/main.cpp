1f1a60344bf0f02d6644d721a5328b54eb5eeb77
eldertree777 <34835912+eldertree777@users.noreply.github.com>
2021-01-04 19:28:57 +0900

    OOP1

---
+++ C++ POCU/07. OOP/OOP/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Vector.h"
+using namespace std;
+
+Vector1::Vector1() :mX(0), mY(0) {}
+Vector1::Vector1(int x, int y) : mX(x), mY(y) {}
+Vector1::~Vector1() {}
+int Vector1::GetX() const {
+	return mX;
+}
+
+	int	main() {
+		
+	}
